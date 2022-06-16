4702e09266bf0dd7185b24b62ebfb4bdbfa285db
AleksChernykh <aleks.chernykh@hotmail.com>
2022-06-16 17:53:40 +0500

    Final Expenses-Manager Project

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+import './NewExpense.css';
+
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpanseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onCancel={stopEditingHandler}
+          onSaveExpenseData={saveExpanseDataHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
